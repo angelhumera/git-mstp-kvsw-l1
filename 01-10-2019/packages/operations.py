3645dea3f3ac640f7b2e8592f09021bc5ba249d7
angelhumera <angelhummuk@gmail.com>
2019-10-22 14:34:20 -0700

    new

---
+++ 01-10-2019/packages/operations.py
@@ -0,0 +1,12 @@
+def addition(a,b):
+    c=a+b
+    print(c)  
+def prime(n):
+    fact=0
+    for i in range(1,n+1):
+        if(n%i==0):
+             fact=fact+1
+    if(fact==2):
+        return True
+    else:
+        return False
